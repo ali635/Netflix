e8fba484c021805e61c6cf074bf054cfacc7cbd3
Mohamed <mohamedali7659@gmail.com>
2020-10-10 23:26:10 +0200

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(window).on('scroll',function () {
+        if($(this).scrollTop() > 100) {
+            $('.navbar').addClass('bg-black-transparent')
+        } else {
+            $('.navbar').removeClass('bg-black-transparent')
+        }
+    });
+});
